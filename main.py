7791262486bf839a2c567a40d3b8d6f3d1057bfc
Akashdeepsingh98 <akashdeepsingh98@gmail.com>
2021-06-19 15:55:28 +0530

    basic sorting by fee

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import heapq
 import queue
 from typing import Dict, List, Set, Tuple
 
+WEIGHT_LIM = 4000000
+
 
 class MempoolTransaction():
-    def __init__(self, txid, fee, weight, parents=[]):
+    def __init__(self, txid: str, fee: int, weight: int, parents=[]) -> None:
         self.txid: str = txid
         self.fee: int = fee
         self.weight: int = weight
@@ -49,7 +51,7 @@ data: Dict[str, MempoolTransaction] = {}  # dictionary with txid as key
 
 def main():
 
-    curweight = 0
+    curweight: int = 0
 
     # read file into data dictionary
     with open('mempool.csv') as f:
@@ -78,20 +80,55 @@ def main():
                 if not parents[i] in data:
                     data[parents[i]] = MempoolTransaction(parents[i], 0, 0)
                 data[parents[i]].children.append(line[0])
-        # print(len(data))
+    print(len(data))
     # print(data['79c51c9d4124c5cbb37a85263748dcf44e182dff83561fa3087f0e9e43f41c33'])
 
     # store sum of each graph and nodes of each graph
-    result: List[Tuple[int, int, List[str]]] = []
+    # ratio of fee by weight, fee, weight, txid list
+    result: List[Tuple[int, int, int, List[str]]] = []
     for txid in data.keys():
         totalfee, totalweight, txidlist = data[txid].getParentGraph()
-        if totalweight < 4_000_000-curweight:
+        # print(totalweight)
+        if totalweight > WEIGHT_LIM:
+            continue
+        if totalweight <= WEIGHT_LIM-curweight:
             curweight += totalweight
-            result.sort(key=lambda x: x[0])
             txidlist.reverse()
-            result.append([totalfee, totalweight, txidlist])
+            result.append(
+                [totalfee/totalweight, totalfee, totalweight, txidlist])
+            result.sort(key=lambda x: x[1])
+            # radix sort so that weight is the main criteria for and next is fee
+            #result.sort(key=lambda x: x[0])
+            #result.sort(key=lambda x: x[1])
         else:
-            pass
+            end = 0
+            curfee = 0
+            curweight2 = 0
+            while True:
+                if end >= len(result) or curfee+result[end][1] > totalfee:
+                    break
+                curfee += result[end][1]
+                curweight2 += result[end][2]
+                end += 1
+            end -= 1
+            if end == -1:
+                pass
+            else:
+                result = result[end+1:]
+                curweight -= curweight2
+                curweight += totalweight
+                result.append(
+                    [totalfee/totalweight, totalfee, totalweight, txidlist])
+                result.sort(key=lambda x: x[1])
+        # print(len(result))
+
+    with open('block.txt', 'w') as f:
+        for item in result:
+            if len(item[3]) > 1:
+                print(item[3])
+            for txid in item[3]:
+                f.write(txid)
+                f.write('\n')
 
 
 if __name__ == '__main__':
